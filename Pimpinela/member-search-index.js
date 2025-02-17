09e119b3f0a8762c8bf9eb760d3ab3eadb8a563b
Rafael <150425334+Rafael-a88@users.noreply.github.com>
2025-02-17 11:16:43 +0100

    Delete Pimpinela directory

--- Pimpinela/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"ClienteUDP","l":"ClienteUDP()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"ClienteUDP","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"ServidorUDP","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"ServidorUDP","l":"ServidorUDP()","u":"%3Cinit%3E()"}];updateSearchResults();
